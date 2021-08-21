e11884f389f0227c196fd39c6eedc24d45191c8d
Loris Bettazza <loris.bettazza@gmail.com>
2021-08-21 07:38:14 +0200

    :sparkles: Add highlight animation when returning to original comment

--- src/content.js
+++ src/content.js
@@ -9,8 +9,15 @@ function isTimestamp(element) {
 function videoPauseHandler() {
   if (!lastClickedTimestamp) return;
 
+  const comment = lastClickedTimestamp.closest('ytd-comment-renderer');
+
   lastClickedTimestamp.scrollIntoView({ block: 'center' });
   lastClickedTimestamp = null;
+
+  if (!comment) return;
+
+  comment.classList.add(HIGHLIGHT_CLASS);
+  setTimeout(() => comment.classList.remove(HIGHLIGHT_CLASS), 1000);
 }
 
 function setupVideo() {
@@ -29,6 +36,7 @@ function documentClickHandler(e) {
 
 /* Variables */
 const YOUTUBE_ID_REGEX = /^\/watch\?v=[\w-]+&t=\d+s$/i;
+const HIGHLIGHT_CLASS = 'yts-highlight-animation';
 let video = null;
 let lastClickedTimestamp = null;
 
